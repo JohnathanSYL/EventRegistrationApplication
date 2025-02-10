fdd41e498744225ee9cae6c0f58a4baa5e08455a
RIC LEE <chuperic@gmail.com>
2025-02-10 14:35:15 +0800

    random push

--- EventRegistrationApplication/Data/EventRegistrationApplicationContext.cs
+++ EventRegistrationApplication/Data/EventRegistrationApplicationContext.cs
@@ -19,6 +19,7 @@ namespace EventRegistrationApplication.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+            
 
             builder.ApplyConfiguration(new CategorySeed());
             builder.ApplyConfiguration(new EventSeed());
